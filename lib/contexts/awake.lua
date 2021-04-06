0d830248e40933a5a00eb734673ebecec69d70f6
Jeremy Arnold <497595+jaggednz@users.noreply.github.com>
2021-04-06 05:54:37 -0500

    Awake Peeking with aux row

--- lib/contexts/awake.lua
+++ lib/contexts/awake.lua
@@ -1,6 +1,7 @@
 local context={
-  prev_mode = nil
-  peeking_at = nil
+  prev_mode = 1,
+  peeking_mode = nil,
+  default_aux_handlers = {}
 }
 
 -- These functions are declared in "Global" script scope so they can
@@ -20,14 +21,42 @@ function ctx_change_mode(new_mode)
 end
 
 function ctx_peek_at_mode(new_mode,peek)
-  if peek then
+  if peek==1 then
     context.prev_mode = mode
     ctx_set_peek_aux_row(new_mode)
+    context.peeking_mode = new_mode
     mode = new_mode
   else
     mode = context.prev_mode
     ctx_restore_default_aux_row()
+    context.peeking_mode = nil
   end
+end
+
+function ctx_aux_row_handler(button,val,device_number)
+   if context.peeking_mode == 1 and val == 1 then
+     new_pos = button-1
+     if edit_ch == 1 and new_pos <= one.length then one.pos = new_pos end
+     if edit_ch == 2 and new_pos <= two.length then two.pos = new_pos end
+   elseif context.peeking_mode == 2 and val == 1 then
+     new_length = button
+     
+     if edit_ch == 1 then 
+       one.length = new_length
+       if edit_pos > one.length then edit_pos = one.length end
+     end
+     
+     if edit_ch == 2 then 
+       two.length = new_length
+       if edit_pos > two.length then edit_pos = two.length end
+     end
+     
+   elseif context.peeking_mode == 3 and val == 1 then
+     new_cutoff = button * 100
+     params:set(snd_params[1],new_cutoff)
+   elseif context.peeking_mode == 4 and val == 1 then
+   end
+   
 end
 
 function ctx_set_peek_aux_row(mode)
@@ -71,12 +100,19 @@ function context.start(devices)
     device.aux.col_handlers[8] = function(self,val) if val == 1 then ctx_change_mode(4) end end
     ]]
     --[[ Advanced mode "peeking", shows that page/mode while the button is held ]]
-    device.aux.col_handlers[5] = function(self,val) then ctx_peek_mode(1,val) end
-    device.aux.col_handlers[6] = function(self,val) then ctx_peek_mode(1,val) end
-    device.aux.col_handlers[7] = function(self,val) then ctx_peek_mode(1,val) end
-    device.aux.col_handlers[8] = function(self,val) then ctx_peek_mode(1,val) end
+    device.aux.col_handlers[5] = function(self,val) ctx_peek_at_mode(1,val) end
+    device.aux.col_handlers[6] = function(self,val) ctx_peek_at_mode(2,val) end
+    device.aux.col_handlers[7] = function(self,val) ctx_peek_at_mode(3,val) end
+    device.aux.col_handlers[8] = function(self,val) ctx_peek_at_mode(4,val) end
     --save default aux row behavior for post peek mode restore
     context.default_aux_handlers[device.device_number] = device.aux.row_handlers
+    
+    for row_btn = 1,8 do
+      device.aux.row_handlers[row_btn] = function(self,val) 
+        ctx_aux_row_handler(row_btn+((self.current_quad-1)*8),val,self.device_number) 
+      end
+    end
+    
 
   end
 end
